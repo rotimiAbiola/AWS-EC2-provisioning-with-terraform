5e0566a0fa57840dabc80dbad82475e9facda0c7
John Abiola <rtmabiola@gmail.com>
2023-09-12 15:11:59 +0100

    Update start-script.sh

--- start-script.sh
+++ start-script.sh
@@ -4,7 +4,7 @@
 sudo apt -y update && sudo apt -y install firewalld
 
 # Modify the default index.html file
-sudo echo “<h3> Hello World from $(hostname -f). Today's date is $(date) </h3>" > /var/www/html/index.html
+sudo echo "<h3> Hello World from $(hostname -f). Today's date is $(date) </h3>" > /var/www/html/index.html
 
 # Whitelist the HTTP port
 sudo firewall-cmd --zone=public --permanent --add-service=http
